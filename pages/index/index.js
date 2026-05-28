f86c952716cc96076981500f582f1b79ab24404d
INNAUHIX <liuzexu0610@163.com>
2026-05-28 18:51:55 +0800

    feat: 完成主页和添加宠物页面开发，包含日期选择器和iOS风格滚轮效果

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,47 @@
 Page({
+  data: {
+    petInfo: null,
+    petImage: '',
+    petName: '',
+    daysWithPet: 0
+  },
+
+  onLoad() {
+    this.loadPetData()
+  },
+
+  onShow() {
+    this.loadPetData()
+  },
+
+  loadPetData() {
+    const pet = wx.getStorageSync('petInfo') || null
+    if (pet) {
+      const daysWithPet = this.calculateDaysWithPet(pet.homeDate)
+      this.setData({
+        petInfo: pet,
+        petImage: pet.petImage || '',
+        petName: pet.petName || '',
+        daysWithPet
+      })
+    } else {
+      this.setData({
+        petInfo: null,
+        petImage: '',
+        petName: '',
+        daysWithPet: 0
+      })
+    }
+  },
+
+  calculateDaysWithPet(homeDate) {
+    if (!homeDate) return 0
+    const home = new Date(homeDate)
+    const today = new Date()
+    const diff = today - home
+    return Math.floor(diff / (1000 * 60 * 60 * 24))
+  },
+
   onAddPetTap() {
     wx.navigateTo({
       url: '/pages/add-pet/index'
